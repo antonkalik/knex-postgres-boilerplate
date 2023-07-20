85da77c7a0203dd655afcb9d9374377efba6b2e3
antonkalik <antonkalik@gmail.com>
2023-07-20 02:05:26 +0200

    models

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { UserModel } from 'src/models/UserModel';
+import { PostModel, PostType } from 'src/models/PostModel';
 import type { Request, Response } from 'express';
 
 export const router = Router();
@@ -20,6 +21,20 @@ router.get('/users', async (req: Request, res: Response) => {
   }
 });
 
-router.get('/posts/:id', (req: Request, res: Response) => {
-  res.status(200).send(`post: ${req.params.id}`);
+router.get('/posts/:id', async (req: Request, res: Response) => {
+  if (!req.params.id) {
+    res.status(400).json({
+      message: 'Bad Request',
+    });
+  }
+
+  try {
+    res.json({
+      data: await PostModel.findById<PostType>(req.params.id),
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Internal Server Error',
+    });
+  }
 });
